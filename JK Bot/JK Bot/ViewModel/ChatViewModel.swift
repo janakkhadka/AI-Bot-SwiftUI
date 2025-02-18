f5f7d3ab8b37c2f82e9970335c0729d2e1b3de20
Janak Khadka <janakkhadka08@gmail.com>
2025-02-18 13:52:42 +0545

    viewmodel added

--- JK Bot/JK Bot/ViewModel/ChatViewModel.swift
+++ JK Bot/JK Bot/ViewModel/ChatViewModel.swift
@@ -6,5 +6,24 @@
 //
 
 import Foundation
+import Combine
 
+class ChatViewModel: ObservableObject {
+    @Published var messages: [Message] = []  //publised le chai view lai notify garxa if they change
+    @Published var userInput: String = ""
+    private let networkManager = NetworkManager()
+    
+    func sendMessage() {
+        let userMessage = Message(text: userInput, isUser: true)
+        messages.append(userMessage)
+        userInput = ""
+        
+        networkManager.sendMessage(message: userMessage.text){ reply in
+            DispatchQueue.main.async {
+                let botMessage = Message(text: reply, isUser: false)
+                self.messages.append(botMessage)
+            }
+        }
+    }
+}
 
